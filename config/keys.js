f60c538644c1089c3e1cb4ede06a4b099db891b6
Dinesh <s3761972@student.rmit.edu.au>
2020-09-08 23:24:14 +1000

    First commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dinesh:e9PqNmRnnkz7fQ9u@rmit-pm-cluster.om6vg.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+//Initialize express
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5003;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));

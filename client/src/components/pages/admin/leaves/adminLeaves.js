3d202f2a829942df69198ace4cd842ad9ea4f1e4
dineshRMIT <s3761972@student.rmit.edu.au>
2020-09-14 17:42:08 +1000

    Fixed layout and breadcrums text

--- client/src/components/pages/admin/leaves/adminLeaves.js
+++ client/src/components/pages/admin/leaves/adminLeaves.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
+import { Layout, Breadcrumb } from "antd";
+import styled from "styled-components";
+
+const { Content } = Layout;
 
 class AdminLeaves extends Component {
   state = {};
   render() {
-    return <div>This is the admin leaves page</div>;
+    return (
+      <StyledLayout>
+        <StyledBreadcrum>
+          <Breadcrumb.Item>Admin</Breadcrumb.Item>
+          <Breadcrumb.Item>Leaves</Breadcrumb.Item>
+        </StyledBreadcrum>
+        <StyledContent>This is the admin leaves page</StyledContent>
+      </StyledLayout>
+    );
   }
 }
+
+const StyledLayout = styled(Layout)`
+  padding: 0 24px 24px;
+`;
+
+const StyledBreadcrum = styled(Breadcrumb)`
+  margin: 16px 0;
+  margin-left: 16px;
+`;
+
+const StyledContent = styled(Content)`
+  margin: 16px 16px;
+  margin-top: 0;
+  padding: 24px;
+  min-height: 280px;
+  background-color: white;
+`;
 
 export default withRouter(AdminLeaves);

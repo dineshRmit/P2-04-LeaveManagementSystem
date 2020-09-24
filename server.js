a481210b447d3481fa7b3d1f073925761eabb6bf
dineshRMIT <s3761972@student.rmit.edu.au>
2020-09-24 13:35:53 +1000

    made changes to deplot to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -27,6 +28,16 @@ mongoose
 
 // Passport middleware
 app.use(passport.initialize());
+
+//Serve static assets of in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Passport config
 require("./config/passport")(passport);
